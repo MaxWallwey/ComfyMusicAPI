f54b2cec1549b4c9eaa92e8695036899ec8c7818
MaxWallwey <127151268+MaxWallwey@users.noreply.github.com>
2023-11-09 20:50:32 +0000

    Add initial API
    Included basic CRUD operations and database storage on internal storage.

---
+++ ComfyMusic/Controllers/SongController.cs
@@ -0,0 +1,62 @@
+using ComfyMusic.Models;
+using ComfyMusic.Servies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComfyMusic.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SongController : ControllerBase
+{
+    public SongController()
+    {
+    }
+
+    [HttpGet]
+    public ActionResult<List<Song>> GetAll() => SongService.GetAll();
+
+    [HttpGet("{id}")]
+    public ActionResult<Song> Get(int id)
+    {
+        var song = SongService.Get(id);
+
+        if(song == null)
+            return NotFound();
+        return song;
+    }
+
+    [HttpPost]
+    public IActionResult Create(Song song)
+    {
+        SongService.Add(song);
+        return CreatedAtAction(nameof(Get), new { id = song.Id }, song);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Song song)
+    {
+        if (id != song.Id)
+            return BadRequest();
+           
+        var existingPizza = SongService.Get(id);
+        if(existingPizza is null)
+            return NotFound();
+   
+        SongService.Update(song);           
+   
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var song = SongService.Get(id);
+
+        if (song is null)
+            return NotFound();
+        
+        SongService.Delete(id);
+
+        return NoContent();
+    }
+}
---
+++ ComfyMusic/Models/Song.cs
@@ -0,0 +1,9 @@
+namespace ComfyMusic.Models;
+
+public class Song
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Album { get; set; }
+    public string? Artist { get; set; }
+}
